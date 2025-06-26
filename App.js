04c8bce00e6612b746b80864c97eb0daa9f71a56
Elankavi M <kavimurugan1204@gmail.com>
2025-06-26 09:30:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+const on_btn = document.getElementById("on");
+const off_btn = document.getElementById("off");
+var img =document.getElementById("bulb");
+
+function bulb_on()
+{
+    img.src ="https://eportfoliojoebarker.wordpress.com/wp-content/uploads/2015/05/light_bulb_on.png";
+}
+
+function bulb_off()
+{
+    img.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8VzU9jq_N8K0iaqs7wjHwSDByx96_uv8WEfpOi91kvUJaxRKtZKd_nsdB76vNz__8osc&usqp=CAU";
+}
